794ab9ae998eef9bd446191c2d3f075d89bbc9e1
amir tutunchi <amirbt@live.com>
2021-09-01 20:40:03 +0430

    Initial Commit

---
+++ MarlySpoon/MarlySpoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarlySpoonApp.swift
+//  MarlySpoon
+//
+//  Created by Amir Tutunchi on 9/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MarlySpoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
