ba04c30e8c25d82e059457129c8f64ff1284b441
Amir Tutunchi <amirbt@live.com>
2021-09-08 11:39:40 +0430

    fix

--- MarlySpoon/Domain/Services/RemoteRecipeServices.swift
+++ MarlySpoon/Domain/Services/RemoteRecipeServices.swift
@@ -10,18 +10,13 @@ import Combine
 import Contentful
 
 class RemoteRecipeServices: RecipeRepository {
+  let client: Client
+  init(client: Client) {
+    self.client = client
+  }
   func getRecipes() -> AnyPublisher<[Recipe], Error> {
-    return Future<[Recipe], Error> { promise in
-      let contentTypeClasses: [EntryDecodable.Type] = [
-        Chef.self,
-        Tag.self,
-        Recipe.self
-      ]
-      let client = Client(
-        spaceId: "kk2bw5ojx476",
-        accessToken: "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c",
-        contentTypeClasses: contentTypeClasses)
-      client.fetchArray(of: Recipe.self) { (res: Result<HomogeneousArrayResponse<Recipe>, Error>) in
+    return Future<[Recipe], Error> {[weak self] promise in
+      self?.client.fetchArray(of: Recipe.self) { (res: Result<HomogeneousArrayResponse<Recipe>, Error>) in
         switch res {
         case .success(let value):
           promise(.success(value.items))
--- MarlySpoon/RecipeListModule/RecipeListModuleBuilder.swift
+++ MarlySpoon/RecipeListModule/RecipeListModuleBuilder.swift
@@ -7,11 +7,21 @@
 
 import Foundation
 import SwiftUI
+import Contentful
 
 /// I use enum type in order to follow lint rule for objects that only host static function it is better use enum rather than classes or structs
 enum RecipeListModuleBuilder {
   static func build() -> some View {
-    let service = RemoteRecipeServices()
+    let contentTypeClasses: [EntryDecodable.Type] = [
+      Chef.self,
+      Tag.self,
+      Recipe.self
+    ]
+    let client = Client(
+      spaceId: "kk2bw5ojx476",
+      accessToken: "7ac531648a1b5e1dab6c18b0979f822a5aad0fe5f1109829b8a197eb2be4b84c",
+      contentTypeClasses: contentTypeClasses)
+    let service = RemoteRecipeServices(client: client)
     let interactor = RecipeListInteractor.init(recipeRepository: service)
     let presenter = RecipeListPresenter.init(interactor: interactor)
     return RecipeListView(presenter: presenter)
